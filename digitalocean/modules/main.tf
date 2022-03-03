ad3cb1726562c4847452a5da211eaba728760f2b
HellO <genykk@ua.fm>
2022-03-03 09:37:23 +0200

    fix ssh key

--- digitalocean/modules/main.tf
+++ digitalocean/modules/main.tf
@@ -15,7 +15,7 @@ provider "digitalocean" {
 
 resource "digitalocean_droplet" "web" {
   # Obtain your ssh_key id number via your account. See Document https://developers.digitalocean.com/documentation/v2/#list-all-keys
-  ssh_keys           = [var.ssh_key.id]
+  ssh_keys           = [file(var.ssh_key).fingerprint]
   image              = var.ubuntu
   region             = var.do_sgp1
   size               = "s-1vcpu-1gb"
@@ -28,7 +28,7 @@ resource "digitalocean_droplet" "web" {
   connection {
       host     = self.ipv4_address
       type     = "ssh"
-      private_key = var.ssh_key
+      private_key = file(var.ssh_key)
       user     = "root"
       timeout  = "2m"
     }
