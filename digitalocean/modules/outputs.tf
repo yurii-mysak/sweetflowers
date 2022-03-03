066218391e9c5cc635ba4e34a5a387d3dcd40af2
HellO <genykk@ua.fm>
2022-03-03 12:53:31 +0200

    fix create

--- digitalocean/modules/outputs.tf
+++ digitalocean/modules/outputs.tf
@@ -1,15 +1,15 @@
 output "IPv4" {
-  value = [digitalocean_droplet.web.ipv4_address]
+  value = digitalocean_droplet.web.*.ipv4_address
 }
 
 output "IPv6" {
-  value = [digitalocean_droplet.web.ipv6_address]
+  value = digitalocean_droplet.web.*.ipv6_address
 }
 
 output "Name" {
-  value = [digitalocean_droplet.web.name]
+  value = digitalocean_droplet.web.*.name
 }
 
 output "ID" {
-  value = [digitalocean_droplet.web.id]
+  value = digitalocean_droplet.web.*.id
 }
