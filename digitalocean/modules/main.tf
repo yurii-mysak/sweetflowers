c7be32db5f1216408ffc14eddd3cd37cd1da1381
HellO <genykk@ua.fm>
2022-03-03 09:23:05 +0200

    fix

--- digitalocean/modules/main.tf
+++ digitalocean/modules/main.tf
@@ -15,7 +15,7 @@ provider "digitalocean" {
 
 resource "digitalocean_droplet" "web" {
   # Obtain your ssh_key id number via your account. See Document https://developers.digitalocean.com/documentation/v2/#list-all-keys
-  ssh_keys           = ["my"]
+  ssh_keys           = [var.ssh_key.id]
   image              = var.ubuntu
   region             = var.do_sgp1
   size               = "s-1vcpu-1gb"
